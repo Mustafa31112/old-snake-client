e8ddba3bb8db74ba81f969afb7205ba6eeccdb45
Mustafa <mustafa31112@gmail.com>
2021-12-04 02:03:25 +0000

    did the module exports and require

---
+++ client.js
@@ -0,0 +1,13 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541     // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,17 +1,17 @@
 const net = require("net");
-
+const connect = require("./client")
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541     // PORT number here,
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "localhost", // IP address here,
+//     port: 50541     // PORT number here,
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  return conn;
-};
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
