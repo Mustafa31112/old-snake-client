80670ae31866c871cfa55889c5298b585e183f22
Mustafa <mustafa31112@gmail.com>
2021-12-05 01:06:34 +0000

    added move up command then setted intervel time

--- client.js
+++ client.js
@@ -3,11 +3,20 @@ const connect = function () {
   const conn = net.createConnection({
     host: "localhost", // IP address here,
     port: 50541     // PORT number here,
-  });
+   });
 
   // interpret incoming data as text
+  // conn.on("connect", () => console.log("connected to game server"));
   conn.setEncoding("utf8");
+  conn.on("connect", () => {
+ conn.write("Name: MUS");
+ 
+})
 
+//  setInterval(() => {
+   
+//    conn.write("Move: Up")
+//  }, 50)
   return conn;
 };
 module.exports = connect;
